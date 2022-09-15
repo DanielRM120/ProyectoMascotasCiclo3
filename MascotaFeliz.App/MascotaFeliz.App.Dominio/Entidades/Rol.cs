542f32e375a424ac45638279b842425f2efbb6b4
JohanaGonza <johanajuliette.gonzalez@uptc.edu.co>
2022-09-15 11:55:11 -0500

    Agregacion Login

--- MascotaFeliz.App/MascotaFeliz.App.Dominio/Entidades/Rol.cs
+++ MascotaFeliz.App/MascotaFeliz.App.Dominio/Entidades/Rol.cs
@@ -8,8 +8,20 @@ namespace MascotaFeliz.App.Dominio
     /// <summary>
     /// Se utilizara para el Login
     /// </summary>
-    public class Rol
+    public class Rol : Usuario
     {
         
+
+        public string Salt { get; set; }
+
+        
+        public string Nombre { get; set; }
+
+       
+        public string Apellidos { get; set; }
+
+        
+        public string Tipo { get; set; }
     }
+    
 }
---
+++ MascotaFeliz.App/MascotaFeliz.App.Persistencia/AppRepositorios/RepositorioRol.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+using MascotaFeliz.App.Dominio;
+using System.Linq;
+
+namespace MascotaFeliz.App.Persistencia
+{
+    
+}
